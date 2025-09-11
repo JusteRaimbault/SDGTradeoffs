5176ad12aecd4c13074680f76a8cd75493307387
Juste Raimbault <juste.raimbault@ign.fr>
2025-09-12 00:40:11 +0300

    analysis

---
+++ Models/FutureTrajectories/analysis.R
@@ -0,0 +1,41 @@
+
+setwd(paste0(Sys.getenv('CS_HOME'),'/SDGTradeoffs/Models/FutureTrajectories'))
+
+library(ggplot2)
+library(dplyr)
+library(readr)
+library(reshape2)
+
+source(paste0(Sys.getenv('CS_HOME'),'/Organisation/Models/Utils/R/plots.R'))
+
+resprefix = 'CALIBRATION_CN_LOCAL_20250911_204557'
+resdir = paste0(Sys.getenv('CS_HOME'),'/SDGTradeoffs/Results/FutureTrajectories/',resprefix,'/');dir.create(resdir,recursive = T, showWarnings = F)
+
+res <- read_csv(paste0('calibration/',resprefix,'.csv'),name_repair='minimal')
+res = res[,3:25]
+names(res)[22:23]=c('logmse','mselog')
+
+ggsave(
+  ggplot(res,aes(x=logmse,y=mselog,color=innovationInnovationDecay))+geom_point()+scale_color_continuous(name='dI')+stdtheme,
+  file=paste0(resdir,'pareto_color-innovationInnovationDecay.png'),width=25,height=20,units='cm'
+)
+  
+table(res$innovationUtilityDistrib)
+# -> 50%~ - can be removed (simpler for the plot)
+
+params = res[,c(-8,-22,-23)]
+# renormalise each param for visu (to check distrib shapes)
+for(j in 1:ncol(params)){params[,j] = (unlist(params[,j]) - mean(unlist(params[,j])))/sd(unlist(params[,j]))}
+d = melt(params,measure.vars = names(params))
+
+ggsave(
+  ggplot(d,aes(variable,value))+geom_violin()+theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)),
+  file=paste0(resdir,'params-standardised-distribs.png'),width=40,height=20,units='cm'
+)
+
+# export boundaries at 1 IQ? or use quantiles ! -> 10-90% = 80% of params, good already!
+# directly in correct format for PSE script
+params = res[,c(-8,-22,-23)]
+apply(params,MARGIN = 2,function(col){paste0(' in (',quantile(col,0.1),',',quantile(col,0.9),')')})  
+# ! check saturated bounds
+
