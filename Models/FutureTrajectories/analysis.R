02b37f078ec40417ef2aedc71ab534d2c910112b
Juste Raimbault <juste.raimbault@ign.fr>
2025-09-12 10:11:08 +0300

    more_experiments

--- Models/FutureTrajectories/analysis.R
+++ Models/FutureTrajectories/analysis.R
@@ -8,8 +8,8 @@ library(reshape2)
 
 source(paste0(Sys.getenv('CS_HOME'),'/Organisation/Models/Utils/R/plots.R'))
 
-#resprefix = 'CALIBRATION_CN_LOCAL_20250911_204557'
-resprefix='CALIBRATION_EU_LOCAL_20250911_182514'
+resprefix = 'CALIBRATION_CN_LOCAL_20250911_204557'
+#resprefix='CALIBRATION_EU_LOCAL_20250911_182514'
 resdir = paste0(Sys.getenv('CS_HOME'),'/SDGTradeoffs/Results/FutureTrajectories/',resprefix,'/');dir.create(resdir,recursive = T, showWarnings = F)
 
 res <- read_csv(paste0('calibration/',resprefix,'.csv'),name_repair='minimal')
@@ -20,7 +20,21 @@ ggsave(
   ggplot(res,aes(x=logmse,y=mselog,color=innovationInnovationDecay))+geom_point()+scale_color_continuous(name='dI')+stdtheme,
   file=paste0(resdir,'pareto_color-innovationInnovationDecay.png'),width=25,height=20,units='cm'
 )
-  
+
+res$mainSubmodel = apply(res[,c("innovationWeight","ecoWeight","coevolWeight")],1,function(row){
+  if(max(row)==row[1]){'innovation'}
+  if(max(row)==row[2]){'economy'}
+  if(max(row)==row[3]){'infrastructure'}
+})
+
+ggsave(
+  ggplot(res,aes(x=logmse,y=mselog,color=mainSubmodel))+geom_point()+scale_color_discrete(name='dI')+stdtheme,
+  file=paste0(resdir,'pareto_color-mainSubmodel.png'),width=25,height=20,units='cm'
+)
+
+
+# Parameters distribution
+
 table(res$innovationUtilityDistrib)
 # -> 50%~ - can be removed (simpler for the plot)
 
@@ -41,6 +55,6 @@ params = res[,c(-8,-22,-23)]
 
 #apply(params,MARGIN = 2,function(col){show(' in (',quantile(col,0.1),',',quantile(col,0.9),')')})  
 for(j in 1:ncol(params)){
-  writeLines(noquote((paste0(names(params)[j],' in (',quantile(unlist(params[,j]),0.1),',',quantile(unlist(params[,j]),0.9),')')))
+  writeLines(noquote((paste0(names(params)[j],' in (',quantile(unlist(params[,j]),0.1),',',quantile(unlist(params[,j]),0.9),'),'))))
 }
 
